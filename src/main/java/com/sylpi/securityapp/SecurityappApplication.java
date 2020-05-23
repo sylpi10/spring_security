5d0bc250f911ad87112fe93db8eec9a53011d81c
sylpi10 <syl.pillet@hotmail.fr>
2020-05-23 15:12:22 +0200

    update

--- src/main/java/com/sylpi/securityapp/SecurityappApplication.java
+++ src/main/java/com/sylpi/securityapp/SecurityappApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class SecurityappApplication {
 
 	public static void main(String[] args) {
+
 		SpringApplication.run(SecurityappApplication.class, args);
 	}
 
